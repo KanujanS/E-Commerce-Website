2852d96027c91a7ca48c0a336b365776492bdeac
KanujanS <kanujansk@gmail.com>
2025-11-12 17:56:54 +0530

    user router api endpoint created

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,2 +1,10 @@
 import express from 'express';
-import { registerUser, loginUser, getUserProfile } from '../controllers/userController.js';
+import { registerUser, loginUser, getUserProfile, adminLogin } from '../controllers/userController.js';
+
+const userRouter = express.Router();
+
+userRouter.post('/register', registerUser);
+userRouter.post('/login', loginUser);
+userRouter.post('/admin', adminLogin);
+
+export default userRouter;
