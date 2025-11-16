f12216002a6bc37710b95ce5066e7c568e849a85
KanujanS <kanujansk@gmail.com>
2025-11-16 22:15:41 +0530

    product controller file initialized

---
+++ backend/controllers/productController.js
@@ -0,0 +1,20 @@
+
+
+// Function for add product
+const addProduct = async (req, res) => {
+    
+}
+// Function for list product
+const listProducts = async (req, res) => {
+
+}
+// Function for remove product
+const removeProduct = async (req, res) => {
+
+}
+// Function for single product info
+const singleProduct = async (req, res) => {
+
+}
+
+export {addProduct, listProducts, removeProduct, singleProduct};
