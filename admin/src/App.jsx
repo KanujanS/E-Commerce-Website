8f5ffaa905aac875799bd244404342724fa5759c
KanujanS <kanujansk@gmail.com>
2025-12-22 22:27:50 +0530

    Added routing and pages for Add, Orders, and List components

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add";
+import Orders from "./pages/Orders";
+import List from "./pages/List";
 
 const App = () => {
   return (
@@ -10,6 +14,13 @@ const App = () => {
         <hr className="border-gray-300"/>
         <div className="flex w-full">
           <Sidebar/>
+          <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+            <Routes>
+              <Route path="/add" element={<Add/>}/>
+              <Route path="/orders" element={<Orders/>}/>
+              <Route path="/list" element={<List/>}/>
+            </Routes>
+          </div>
         </div>
       </>
     </div>
