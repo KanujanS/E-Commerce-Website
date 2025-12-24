faa34d5afcd63528075fa9335e94674361598ced
KanujanS <kanujansk@gmail.com>
2025-12-24 22:07:41 +0530

    Enhance Add component with form for product details and image upload

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,8 +1,81 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Add = () => {
   return (
-    <div>Add</div>
+    <form className='flex flex-col w-full items-start gap-3'>
+        <div>
+            <p className='mb-2'>Upload Image</p>
+            <div className='flex gap-2'>
+                <label htmlFor="image1" className='cursor-pointer'>
+                    <img className='w-20' src={assets.upload_area} alt="" />
+                    <input type="file" id='image1' hidden/>
+                </label>
+                <label htmlFor="image2" className='cursor-pointer'>
+                    <img className='w-20' src={assets.upload_area} alt="" />
+                    <input type="file" id='image2' hidden/>
+                </label>
+                <label htmlFor="image3" className='cursor-pointer'>
+                    <img className='w-20' src={assets.upload_area} alt="" />
+                    <input type="file" id='image3' hidden/>
+                </label>
+                <label htmlFor="image4" className='cursor-pointer'>
+                    <img className='w-20' src={assets.upload_area} alt="" />
+                    <input type="file" id='image4' hidden/>
+                </label>
+            </div>
+        </div>
+        <div className='w-full'>
+            <p className='mb-2'>Product Name</p>
+            <input type="text" className='w-full max-w-125 px-3 py-2' placeholder='Type here' required/>
+        </div>
+        <div className='w-full'>
+            <p className='mb-2'>Product description</p>
+            <textarea type="text" className='w-full max-w-125 px-3 py-2 resize-none' placeholder='Write content here' required/>
+        </div>
+        <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
+            <div>
+                <p className='mb-2'>Product category</p>
+                <select className='w-full px-3 py-2'>
+                    <option value="Men">Men</option>
+                    <option value="Women">Women</option>
+                    <option value="Kids">Kids</option>
+                </select>
+            </div>
+            <div>
+                <p className='mb-2'>Subcategory</p>
+                <select className='w-full px-3 py-2'>
+                    <option value="Topwear">Topwear</option>
+                    <option value="Bottomwear">Bottomwear</option>
+                    <option value="Winterwear">Winterwear</option>
+                </select>
+            </div>
+            <div>
+                <p className='mb-2'>Product price</p>
+                <input className='w-full px-3 py-2 sm:w-30' type="Number" placeholder='0.00' required/>
+            </div>
+        </div>
+        <div>
+            <p className='mb-2'>Product sizes</p>
+            <div className='flex gap-3'>
+                <div>
+                    <p className='bg-slate-200 px-3 py-1 cursor-pointer'>S</p>
+                </div>
+                <div>
+                    <p className='bg-slate-200 px-3 py-1 cursor-pointer'>M</p>
+                </div>
+                <div>
+                    <p className='bg-slate-200 px-3 py-1 cursor-pointer'>L</p>
+                </div>
+                <div>
+                    <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XL</p>
+                </div>
+                <div>
+                    <p className='bg-slate-200 px-3 py-1 cursor-pointer'>XXL</p>
+                </div>
+            </div>
+        </div>
+    </form>
   )
 }
 
