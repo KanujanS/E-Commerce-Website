682682069ea9fdb27d2a8ca073cf63b645e1bb18
KanujanS <kanujansk@gmail.com>
2026-02-16 20:40:07 +0530

    Add cart functionality with routes and controller methods

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
 import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
 
 // App config
 const app = express();
@@ -19,9 +20,11 @@ app.use(cors());
 // api endpoints
 app.use('/api/user', userRouter);
 app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter);
 app.get('/',(req,res)=>{
     res.send("API Working")
 });
+
 
 // listen
 app.listen(port, ()=> console.log(`Server is running on port ${port}`)
