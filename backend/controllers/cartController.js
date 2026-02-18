f4eb39d744b2df47e59e7c777ffd5a6ffda150a2
KanujanS <kanujansk@gmail.com>
2026-02-18 22:09:45 +0530

    Fix getUserCart function to handle errors and return cart data

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -41,7 +41,15 @@ const updateCart = async (req, res) => {
 }
 // Get user cart data
 const getUserCart = async (req, res) => {
-
+    try {
+        const { userId } = req.body;
+        const userData = await userModel.findById(userId);
+        let cartData = userData.cartData;
+        res.json({success: true, cartData})
+    } catch (error) {
+        console.log(error);
+        res.json({success: false, message: error.message})
+    }
 }
 
 export { addToCart, updateCart, getUserCart }
