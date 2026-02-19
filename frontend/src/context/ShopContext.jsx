ae615f41fed011693fb75514043799d19c8bcbf2
KanujanS <kanujansk@gmail.com>
2026-02-19 23:47:15 +0530

    Fix import statement for userModel and enhance cart functionality with API calls for adding, updating, and retrieving cart items

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -38,6 +38,15 @@ const ShopContextProvider = (props) => {
         }
         setCartItems(cartData);
         toast.success("Product added to cart");
+
+        if (token) {
+            try {
+                await axios.post(backendUrl + '/api/cart/add', {itemId, size}, {headers: {token}});
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message);
+            }
+        }
     }
 
     const getCartCount = () => {
@@ -60,7 +69,15 @@ const ShopContextProvider = (props) => {
         let cartData = structuredClone(cartItems) ;
         cartData[itemId][size] = quantity;
         setCartItems(cartData);
-        toast.success('Product removed from cart');
+        toast.success('Product quantity updated');
+        if (token) {
+            try {
+                await axios.post(backendUrl + '/api/cart/update', {itemId, size, quantity}, {headers: {token}});
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message);
+            }
+        }
     }
 
     const getCartAmount =() =>{
@@ -94,6 +111,18 @@ const ShopContextProvider = (props) => {
         }
     }
 
+    const getUserCart = async (token) => {
+        try {
+            const response = await axios.post(backendUrl + '/api/cart/get', {}, {headers: {token}});
+            if (response.data.success) {
+                setCartItems(response.data.cartData);
+            } 
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    }
+
     useEffect (()=> {
         getProductsData()
     },[])
@@ -101,6 +130,7 @@ const ShopContextProvider = (props) => {
     useEffect(()=>{
         if (!token && localStorage.getItem('token')) {
             setToken(localStorage.getItem('token'));
+            getUserCart(localStorage.getItem('token'))
         }
     },[])
 
