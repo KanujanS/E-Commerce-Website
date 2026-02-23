6a640f23a7a68950528715784f079128764415a4
KanujanS <kanujansk@gmail.com>
2026-02-23 23:25:53 +0530

    Add order controller methods for handling various order operations

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,23 @@
+
+// Placing orders using COD Method
+const placeOrder = async (req, res) => {
+    
+}
+// Placing orders using PayPal Method
+const placeOrderPaypal = async (req, res) => {
+
+}
+// All orders data for admin panel
+const allOrders = async (req, res) => {
+    
+}
+// User order data for frontend
+const userOrders = async (req, res) => {
+
+}
+// Update order status from admin panel
+const updateStatus = async (req, res) => {
+
+}
+
+export { placeOrder, placeOrderPaypal, allOrders, userOrders, updateStatus }
