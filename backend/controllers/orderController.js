ea404a5aeeff0caa3f15dbceed33781c28673e07
KanujanS <kanujansk@gmail.com>
2026-02-26 00:02:02 +0530

    Implemented order placement functionality with COD method

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,7 +1,26 @@
-
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
 // Placing orders using COD Method
 const placeOrder = async (req, res) => {
-    
+    try {
+        const { userId, items, amount, address } = req.body;
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod : "COD",
+            payment : false,
+            date : Date.now()
+        }
+        const newOrder = new orderModel(orderData);
+        await newOrder.save();
+        await userModel.findByIdAndUpdate(userId, { cartData : {} })
+        res.json({ success : true, message : "Order placed successfully" })
+    } catch (error) {
+        console.log(error);
+        res.json({ success : false, message : error.message })
+    }
 }
 // Placing orders using PayPal Method
 const placeOrderPaypal = async (req, res) => {
