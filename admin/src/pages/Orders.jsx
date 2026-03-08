05dc8cb5ccf4c4710aa1d6ea7d6e83a614ea8c27
KanujanS <kanujansk@gmail.com>
2026-03-08 23:09:04 +0530

    Refactored order fetching logic in Orders component in admin panel to store data

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import { useState } from 'react'
 import axios from 'axios'
 import {backendUrl} from '../App'
+import { toast } from 'react-toastify'
 
 const Orders = ({token}) => {
   const [orders,setOrders] = useState([])
@@ -12,10 +13,14 @@ const Orders = ({token}) => {
     }
     try {
       const response = await axios.post(backendUrl+'/api/order/list', {}, {headers: {token}})
-      
+      if (response.data.success) {
+        setOrders(response.data.orders);
+      } else {
+        toast.error(response.data.message);
+      }
       
     } catch (error) {
-      
+      toast.error(error.message);
     }
   }
   useEffect(() => {
