81a003b90af87695778f8f0f0bb81f8682fef200
KanujanS <kanujansk@gmail.com>
2026-03-14 23:49:39 +0530

    Refactored Orders component to display order details

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -1,58 +1,95 @@
-import React from 'react'
-import { useEffect } from 'react'
-import { useState } from 'react'
-import axios from 'axios'
-import {backendUrl} from '../App'
-import { toast } from 'react-toastify'
-import { assets } from '../assets/assets'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { backendUrl, currency } from "../App";
+import { toast } from "react-toastify";
+import { assets } from "../assets/assets";
 
-const Orders = ({token}) => {
-  const [orders,setOrders] = useState([])
+const Orders = ({ token }) => {
+  const [orders, setOrders] = useState([]);
   const fetchAllOrders = async () => {
     if (!token) {
       return null;
     }
     try {
-      const response = await axios.post(backendUrl+'/api/order/list', {}, {headers: {token}})
+      const response = await axios.post(
+        backendUrl + "/api/order/list",
+        {},
+        { headers: { token } },
+      );
       if (response.data.success) {
         setOrders(response.data.orders);
       } else {
         toast.error(response.data.message);
       }
-      
     } catch (error) {
       toast.error(error.message);
     }
-  }
+  };
   useEffect(() => {
     fetchAllOrders();
-  },[token])
+  }, [token]);
   return (
     <div>
       <h3>Order Page</h3>
       <div>
-        {
-          orders.map((order,index) => (
-            <div key={index}>
-              <img src={assets.parcel_icon} alt="" />
+        {orders.map((order, index) => (
+          <div key={index}>
+            <img src={assets.parcel_icon} alt="" />
+            <div>
               <div>
-                {
-                  order.items.map((item,index) => {
-                    if (index === order.items.length - 1) {
-                      return <p key={index}>{item.name} X {item.quantity} <span> {item.size} </span></p>
-                    } else {
-                      return <p key={index}>{item.name} X {item.quantity} <span> {item.size}, </span></p>
-                    }
-                  })
-                }
+                {order.items.map((item, index) => {
+                  if (index === order.items.length - 1) {
+                    return (
+                      <p key={index}>
+                        {item.name} X {item.quantity} <span> {item.size} </span>
+                      </p>
+                    );
+                  } else {
+                    return (
+                      <p key={index}>
+                        {item.name} X {item.quantity}{" "}
+                        <span> {item.size}, </span>
+                      </p>
+                    );
+                  }
+                })}
               </div>
-              <p>{order.address.firstName + " " +order.address.lastName}</p>
+              <p>{order.address.firstName + " " + order.address.lastName}</p>
+              <div>
+                <p>{order.address.street + ","}</p>
+                <p>
+                  {order.address.city +
+                    ", " +
+                    order.address.state +
+                    ", " +
+                    order.address.country +
+                    ", " +
+                    order.address.zipcode}
+                </p>
+              </div>
+              <p>{order.address.phone}</p>
             </div>
-          ))
-        }
+            <div>
+              <p>Items : {order.items.length}</p>
+              <p>Method : {order.paymentMethod}</p>
+              <p>Payment : {order.payment ? "Paid" : "Not Paid"}</p>
+              <p>Date : {new Date(order.date).toLocaleDateString()}</p>
+            </div>
+            <p>{currency} {order.amount}</p>
+            <select>
+              <option value="Order Placed">Order Placed</option>
+              <option value="Packaging">Packaging</option>
+              <option value="Shipped">Shipped</option>
+              <option value="Out for Delivery">Out for Delivery</option>
+              <option value="Delivered">Delivered</option>
+            </select>
+          </div>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Orders
+export default Orders;
